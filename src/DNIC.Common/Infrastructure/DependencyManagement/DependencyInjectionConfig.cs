f92924002202b0dae5b42fe77676a9befdf3dc49
fanzongxuan@perfay.com <fanzongxuan@perfay.com>
2018-04-10 15:25:03 +0800

    定义配置接口并统一服务

---
+++ src/DNIC.Common/Infrastructure/DependencyManagement/DependencyInjectionConfig.cs
@@ -0,0 +1,34 @@
+﻿using DNIC.Common.Cache;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNIC.Common.Infrastructure.DependencyManagement
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void Inject(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<PerRequestCacheManager>();
+            services.Configure<RedisOptions>(configuration.GetSection("RedisOptions"));
+            //cache
+            var redisEnable = configuration.GetValue<bool>("RedisOptions:Enable");
+            if (redisEnable)
+            {
+                services.AddDistributedRedisCache(options =>
+                {
+                    options.Configuration = configuration.GetValue<string>("RedisOptions:Configration");
+                    options.InstanceName = configuration.GetValue<string>("RedisOptions:InstanceName");
+                });
+                services.AddScoped<IRedisConnectionWrapper, RedisConnectionWrapper>();
+                services.AddScoped<ICacheManager, DistributeCacheManager>();
+            }
+            else
+            {
+                services.AddMemoryCache();
+            }
+        }
+    }
+}
---
+++ src/DNIC.Common/Interface/ISetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNIC.Common.Interface
+{
+    /// <summary>
+    /// 配置接口
+    /// </summary>
+    public interface ISetting
+    {
+    }
+}
