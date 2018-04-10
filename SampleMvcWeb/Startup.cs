d48b72f581c08367025bf24f47563f6fd6c181c8
fanzongxuan@perfay.com <fanzongxuan@perfay.com>
2018-04-10 16:20:26 +0800

    重命名服务注入

--- SampleMvcWeb/Startup.cs
+++ SampleMvcWeb/Startup.cs
@@ -22,7 +22,7 @@ namespace SampleMvcWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Inject(Configuration);
+            services.AddCommon(Configuration);
             services.AddMvc();
         }
 
